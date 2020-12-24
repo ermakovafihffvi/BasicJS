4268296f0b13a1565125b5a315a4b3bfbb9fe98e
Vera <veraermakova.fihffvi@gmail.com>
2020-12-24 20:32:34 +0300

    добавлена папка с выполненными заданиями и файл с описанием самих заданий

---
+++ hw2/Solution3/app.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+let a = +prompt("Input а: ");
+let b = +prompt("Input b: ");
+
+if (a >= 0 && b >= 0) {
+    alert("Both values are positive. Their difference is " + (a - b));
+} else if (a <= 0 && b <= 0) {
+    alert("Both values are negative. Their product is " + (a * b));
+} else if ((a * b) <= 0) {
+    let c = a + b;
+    alert("This values have different signs. Their sum is " + c);
+} else {
+    alert("Inputed values are not numbers.");
+}
