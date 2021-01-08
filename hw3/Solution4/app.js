85b80849b73ecabbcd3fd61ba6aaed6a25d4bcd6
Vera <veraermakova.fihffvi@gmail.com>
2021-01-08 23:30:30 +0300

    homework3
    Добавлено 6 решений. Текст заданий в   pdf файле.

---
+++ hw3/Solution4/app.js
@@ -0,0 +1,42 @@
+﻿"use strict";
+
+const products = [
+    {
+        id: 3,
+        price: 127,
+        photos: [
+        "1.jpg",
+        "2.jpg",
+        ]
+    },
+    {
+        id: 5,
+        price: 499,
+        photos: []
+    },
+    {
+        id: 10,
+        price: 26,
+        photos: [
+        "3.jpg"
+        ]
+    },
+    {
+        id: 8,
+        price: 78,
+    }
+];
+
+let productsList = products.filter(function (product) {
+    if (product.photos != null) {
+        if (product.photos.length != 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    } else {
+        return 0;
+    }
+});
+
+console.log(productsList);
---
+++ hw3/Solution6/app.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+
+for (let i = 1; i <= 20; i++) {
+    let arr = [];
+    for (let j = 1; j <= i; j++) {
+        arr.push("x");
+    }
+    let finalString = arr.join('');
+    console.log(finalString);
+}
